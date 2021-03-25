1bd90fb6c91b1069deee923163049636d0a57326
451083622 <451083622@qq.com>
2021-03-25 11:32:23 +0800

    测试依赖最新版

---
+++ app/src/main/java/com/example/loginmodule/login/TestConstant.java
@@ -0,0 +1,14 @@
+package com.example.loginmodule.login;
+
+/**
+ * copyright (C), 2021, 运达科技有限公司
+ * fileName TestConstant
+ *
+ * @author 王玺权
+ * date 2021-03-25 11:30
+ * description
+ * history
+ */
+public class TestConstant {
+    public static final String test="success";
+}
